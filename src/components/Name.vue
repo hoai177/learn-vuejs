d07451f00c7abfc8a8da282b01d0ce3e8754c0ba
Hoai <vothihoai170798@gmail.com>
2021-07-21 14:34:30 +0700

    add button count

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>Learn vuejs</h1>
+  <button @click="state.count++">Increment {{ state.count }}</button>
 </template>
+
+<script setup>
+import {reactive } from 'vue'
+
+
+const state = reactive({ count: 0 })
+</script>
+
