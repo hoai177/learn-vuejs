286535ccc4974da978b6676c35653d6b723c7a99
Hoai <vothihoai170798@gmail.com>
2021-07-26 09:31:40 +0700

    add button decrement

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -1,11 +1,15 @@
 <template>
   <h1>Learn vuejs</h1>
   <button @click="increment">Increment {{ this.count }}</button>
+  <Button></Button>
 </template>
 
 <script>
-
+import Button from "./Button.vue"
  export default {
+    components: {
+      Button
+    },
     data () {
       return {
        count: 0
