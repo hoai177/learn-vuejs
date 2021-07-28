89dad2c0fcdaa7094b7f3e70e28966c9ff49abe0
Hoai <vothihoai170798@gmail.com>
2021-07-28 10:11:26 +0700

    add array

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,3 +1,24 @@
 <template>
   <h1>Todo Page</h1>
+  <ul>
+    <li v-for="item in items" v-bind:key="item">
+      {{ item }}
+    </li>
+  </ul>
 </template>
+
+<script>
+export default {
+  data () {
+      return {
+       items: ['Lam viec', 'Tap the duc', 'Doc sach']
+      }
+    },
+}
+</script>
+
+<style>
+  ul {
+    display: inline-block;
+  }
+</style>
