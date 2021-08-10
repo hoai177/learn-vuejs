266a4169c72a3269c1406b6dc9eb135367e0ff1a
Hoai <vothihoai170798@gmail.com>
2021-08-10 15:01:01 +0700

    delete code

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script setup>
-import Home from './components/Home.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
